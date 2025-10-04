e123809d458dfe5fdb093c21353dc01b80c399ed
Filippo Venturini <filippoventurini00@gmail.com>
2025-10-04 19:35:17 +0200

    feat(satellite): import model and scale

--- frontend/src/core/objects/space.js
+++ frontend/src/core/objects/space.js
@@ -4,6 +4,11 @@ export function createSpace() {
   const loader = new THREE.TextureLoader();
   const texture = loader.load('/textures/stars.jpg');
 
+  // Ripetizione della texture
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(4, 4); // numero di ripetizioni orizzontale e verticale
+
   const geometry = new THREE.SphereGeometry(90, 64, 64);
   const material = new THREE.MeshBasicMaterial({
     map: texture,
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
 import { createScene } from './core/scene.js';
 import { createEarth } from './core/objects/earth.js';
 import { createSpace } from './core/objects/space.js';
-import { addCraterVertexColor } from './core/objects/crater.js';
+import { createSatellite } from './core/objects/satellite.js';
 import { startRenderLoop } from './core/renderer.js';
 import * as THREE from 'three';
 import { createAsteroid } from './core/objects/asteroid.js';
 
 const { scene, camera, renderer } = createScene();
+
+createSatellite().then((satellite) => {
+  scene.add(satellite);
+}).catch((err) => {
+  console.error(err);
+});
 
 const earthMesh = createEarth();
 scene.add(earthMesh);
